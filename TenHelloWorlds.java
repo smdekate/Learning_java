762a099ad1349a800e32b39073cc84889eb34039
smdekate <90138014+smdekate@users.noreply.github.com>
2022-02-09 15:17:10 +0530

    Add files via upload

---
+++ TenHelloWorlds.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Compilation: javac TenHelloWorlds.java
+ * Execution: java TenHelloWorlds
+ *
+ * Prints "Hello, World" ten times
+ ******************************************************************************/
+
+public class TenHelloWorlds {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+}
