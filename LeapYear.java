16a28ccd1778e6c6b08af864bbfede933aaa8dc7
smdekate <90138014+smdekate@users.noreply.github.com>
2022-02-10 15:25:53 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Compilation: javac LeapYear.java
+ * Execution: java LeapYear n
+ *
+ * Prints true if n corresponds to a Leap Year, and false otherwise.
+ * Assumes n >= 1582, corresponding to a year in the Gregorian calendar.
+ ******************************************************************************/
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = Integer.parseInt(args[0]);
+        boolean isLeapYear;
+
+        // divisible by 4
+        isLeapYear = (year % 4 == 0);
+
+        // divisible by 4 and not 100
+        isLeapYear = isLeapYear && (year % 100 != 0);
+
+        // divisible by 4 an not 100 unless divisible by 400
+        isLeapYear = isLeapYear || (year % 400 == 0);
+
+        System.out.println(isLeapYear);
+    }
+}
