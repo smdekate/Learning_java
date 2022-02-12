5cb3720062ba77b223623ba94274aeaa08c7b268
smdekate <90138014+smdekate@users.noreply.github.com>
2022-02-12 12:19:09 +0530

    Add files via upload

---
+++ ThreeSort.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Compilation: javac ThreeSort.java
+ * Execution: java ThreeSort a b c
+ *
+ * Read 3 integer values from the command-line and print them
+ * int ascending order.
+ ******************************************************************************/
+
+public class ThreeSort {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        // compute stats
+        int min = Math.min(a, Math.min(b, c));
+        int max = Math.max(a, Math.max(b, c));
+        int median = a + b + c - min - max;
+
+        // print stats
+        System.out.println(min);
+        System.out.println(median);
+        System.out.println(max);
+    }
+}
