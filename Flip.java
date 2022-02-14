47c2976df364aa93dff2fa80b49806b3e7aead6a
smdekate <90138014+smdekate@users.noreply.github.com>
2022-02-14 11:34:21 +0530

    Add files via upload

---
+++ Flip.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Compilation: javac Flip.java
+ * Execution: java Flip
+ *
+ * Simulate a fair coin flip and print out "Heads" or "Tails" accordingly.
+ ******************************************************************************/
+
+public class Flip {
+    public static void main(String[] args) {
+        // Math.random() return a value between 0.0 and 1.0
+        // so it is heads or tails 50% of the time
+        if (Math.random() < 0.5) System.out.println("Heads");
+        else System.out.println("Tails");
+    }
+}
