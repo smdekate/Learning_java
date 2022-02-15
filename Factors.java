8575299454f55df1cf55558e7e93e81a54a8f931
smdekate <90138014+smdekate@users.noreply.github.com>
2022-02-15 15:52:16 +0530

    Add files via upload

---
+++ Factors.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Compilation: javac Factors.java
+ * Execution: java Factors n
+ *
+ * Computes the prime factorization of n using brute force.
+ *
+ * Remakes
+ * -------
+ *  - tests factor*factor <= n instead of factor <= n for efficiency.
+ ******************************************************************************/
+
+public class Factors {
+    public static void main(String[] args) {
+
+        // command-line argument
+        long n = Long.parseLong(args[0]);
+
+        System.out.print("The prime factorization of " + n + " is : ");
+
+        // for each potential factor
+        for (long factor = 2; factor * factor <= n; factor++) {
+
+            // if factor is factor of n, repeatedly divide it out
+            while (n % factor == 0) {
+                System.out.print(factor + " ");
+                n = n / factor;
+            }
+        }
+
+        // if biggest factor occurs only once, n > 1
+        if (n > 1) System.out.println(n);
+        else System.out.println();
+    }
+}
